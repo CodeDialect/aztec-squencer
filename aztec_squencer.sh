f09674d8f18d5f69d91e7985104be95be7d47785
CodeDialect <94617063+CodeDialect@users.noreply.github.com>
2025-06-07 11:59:19 +0530

    Update aztec_squencer.sh

--- aztec_squencer.sh
+++ aztec_squencer.sh
@@ -87,7 +87,7 @@ fi
 # Setup Network
 # ===============================
 echo -e "${BLUE}[INFO] Setting up Aztec alpha-testnet environment...${RESET}"
-aztec-up -v 0.87.6
+aztec-up latest
 
 # ===============================
 # Start Node
